04578347cdf23a8dd59e0cf7dbf4cb0c6de94434
Bohdan Yurov <bohdan@yurov.me>
2024-02-07 13:55:38 +0200

    Fixing examples, publishing preparation

--- examples/core_flavors/versions.tf
+++ examples/core_flavors/versions.tf
@@ -1,9 +1,5 @@
 terraform {
   required_providers {
-    random = {
-      source  = "hashicorp/random"
-      version = "~> 3.6.0"
-    }
     hyperstack = {
       source  = "nexgen/hyperstack"
       version = "~> 0.0.1"
