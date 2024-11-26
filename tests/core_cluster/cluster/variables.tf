39cf0ef76af5adc1822df40834fc0496b811aed9
Sebastian Panman de Wit <167725923+sebastianpdw-nexgencloud@users.noreply.github.com>
2024-11-26 18:06:48 +0800

    Simplifying tests + fixing data source hyperstack_core_volumes (#23)
    This PR simplifies the test setup by reducing the number of Hyperstack
    resources.
    Additionally, it fixes a current bug in the data source
    `hyperstack_core_volumes`.
    
    I have ran all tests successfully after building the provider

--- tests/core_cluster/cluster/variables.tf
+++ tests/core_cluster/cluster/variables.tf
@@ -39,15 +39,14 @@ variable "clusters" {
     image_type = optional(string, "Ubuntu")
     image_version = optional(string, "Server 22.04 LTS R535 CUDA 12.2")
   }))
-
   default = {
     "cpu-2" = {
       enabled    = true
       validation = {}
-      node_count = 2
+      node_count = 1
       node_flavor = {
         gpu_name  = ""
-        cpu_count = 8
+        cpu_count = 4
       }
     }
     "a6000-2" = {
@@ -58,42 +57,6 @@ variable "clusters" {
       node_count = 2
       node_flavor = {
         gpu_name  = "RTX-A6000"
-        gpu_count = 1
-      }
-      image_version = "Server 22.04 LTS R535 CUDA 12.2"
-    }
-    "l40-2" = {
-      enabled    = true
-      validation = {
-        gpu = true
-      }
-      node_count = 2
-      node_flavor = {
-        gpu_name  = "L40"
-        gpu_count = 1
-      }
-      image_version = "Server 22.04 LTS R535 CUDA 12.2"
-    }
-    "a100x1-2" = {
-      enabled    = true
-      validation = {
-        gpu = true
-      }
-      node_count = 2
-      node_flavor = {
-        gpu_name  = "A100-80G-PCIe"
-        gpu_count = 1
-      }
-      image_version = "Server 22.04 LTS R535 CUDA 12.2"
-    }
-    "h100x1-2" = {
-      enabled    = true
-      validation = {
-        gpu = true
-      }
-      node_count = 2
-      node_flavor = {
-        gpu_name  = "H100-80G-PCIe"
         gpu_count = 1
       }
       image_version = "Server 22.04 LTS R535 CUDA 12.2"
